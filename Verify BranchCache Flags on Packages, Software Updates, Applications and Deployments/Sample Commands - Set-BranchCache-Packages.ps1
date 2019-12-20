cb4f7241c0870bb1676bde93e0b370aa8baa09af
arwidmark <arwidmark@gmail.com>
2019-12-19 18:54:30 -0600

    Added BranchCache Flags Scripts

---
+++ Verify BranchCache Flags on Packages, Software Updates, Applications and Deployments/Sample Commands - Set-BranchCache-Packages.ps1
@@ -0,0 +1,21 @@
+﻿# Gather info about all packages
+.\Set-BranchCache-Packages.ps1 -SiteServer CM01 -Mode Gather
+
+# Disable BranchCache on all packages that have content
+.\Set-BranchCache-Packages.ps1 -SiteServer CM01 -Mode Disable
+
+# Enable BranchCache on all packages that have content
+.\Set-BranchCache-Packages.ps1 -SiteServer CM01 -Mode Enable
+
+
+# Gather info about individual package(s), wild cards supported
+.\Set-BranchCache-Packages.ps1 -SiteServer CM01 -Mode Gather -PackageName "1 GB File 001"
+.\Set-BranchCache-Packages.ps1 -SiteServer CM01 -Mode Gather -PackageName "*File*"
+
+# Disable BranchCache on individual package(s), wild cards supported
+.\Set-BranchCache-Packages.ps1 -SiteServer CM01 -Mode Disable -PackageName "1 GB File 001"
+.\Set-BranchCache-Packages.ps1 -SiteServer CM01 -Mode Disable -PackageName "*File*"
+
+# Enable BranchCache on individual package(s), wild cards supported
+.\Set-BranchCache-Packages.ps1 -SiteServer CM01 -Mode Enable -PackageName "1 GB File 001"
+.\Set-BranchCache-Packages.ps1 -SiteServer CM01 -Mode Enable -PackageName "*File*"
---
+++ Verify BranchCache Flags on Packages, Software Updates, Applications and Deployments/Sample Commands - Set-BranchCache-SoftwareUpdates.ps1
@@ -0,0 +1,8 @@
+﻿# Gather info about all software updates
+.\Set-BranchCache-SoftwareUpdates.ps1 -SiteServer CM01 -Mode Gather
+
+# Disable BranchCache on all software updates
+.\Set-BranchCache-SoftwareUpdates.ps1 -SiteServer CM01 -Mode Disable
+
+# Enable BranchCache on all software updates
+.\Set-BranchCache-SoftwareUpdates.ps1 -SiteServer CM01 -Mode Enable
